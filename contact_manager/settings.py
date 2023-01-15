75fe64de0fc02382cb4d4b96b9d9ee05c76152c7
tappmax <tappmax.create@gmail.com>
2023-01-14 19:22:02 -0600

    add settings file

--- contact_manager/settings.py
+++ contact_manager/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import json
 from pathlib import Path
+
+settingsFile = open('settings.json')
+SETTINGS = json.load(settingsFile)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@$)#bj78hib+f+jnb9^dh8r-_cm)t%z99gy^)&(n5$v@u1q(^s'
+SECRET_KEY = SETTINGS.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
